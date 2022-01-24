58397895146cbc855eb719eb7c29e6da22148c9a
John I <93291968+ionutziv7@users.noreply.github.com>
2022-01-24 21:35:42 +0200

    Update PlusMinus.cpp

--- Algorithms/PlusMinus/PlusMinus.cpp
+++ Algorithms/PlusMinus/PlusMinus.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
 
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    double pos, neg, zeros;
+    int size = arr.size();
+    pos = neg = zeros = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        if (arr[i] > 0) pos++;
+        else if (arr[i] < 0) neg++;
+        else zeros++;
+    }
+    cout << setprecision(6) << fixed << pos / size << endl;
+    cout << neg / size << endl;
+    cout << zeros / size << endl;
+
+    return 0;
+
+}
