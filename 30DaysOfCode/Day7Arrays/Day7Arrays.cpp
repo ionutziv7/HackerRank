7cb98ebe35a863060f40d85802b92c79b34a920e
John I <93291968+ionutziv7@users.noreply.github.com>
2022-01-28 13:31:59 +0200

    Update Day7Arrays.cpp

--- 30DaysOfCode/Day7Arrays/Day7Arrays.cpp
+++ 30DaysOfCode/Day7Arrays/Day7Arrays.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <queue>
+#include <limits>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 
-int main()
+void versionOne()
 {
-    int n;
+     int n;
     cin>>n;
     int arr[n];
+    
     for(int i = 0; i<n;i++)
     {
         cin>>arr[i];
     }
+    
     for(int i = n - 1; i>=0;i--)
     {
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
+}
+
+void versionTwo()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    for(int i=0;i<n;i++)
+    {
+            cin>>arr[i];
+    }
+    for(int i = n-1; i >= 0 ;i--)
+    {
+        cout<< arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    versionOne();
+    versionTwo();
     
 	return 0;
 }
