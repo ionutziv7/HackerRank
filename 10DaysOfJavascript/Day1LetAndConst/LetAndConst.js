c13054e96e586aa1e6c7cf0c2c49add280d011ff
John I <93291968+ionutziv7@users.noreply.github.com>
2022-01-28 18:56:55 +0200

    Create LetAndConst.js
    Task
    
    Declare a constant variable, , and assign it the value Math.PI. You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
    Read a number, , denoting the radius of a circle from stdin.
    Use  and  to calculate the  and  of a circle having radius .
    Print  as the first line of output and print  as the second line of output.
    Input Format
    
    A single integer, , denoting the radius of a circle.
    
    Constraints
    
     is a floating-point number scaled to at most  decimal places.
    Output Format
    
    Print the following two lines:
    
    On the first line, print the  of the circle having radius .
    On the second line, print the  of the circle having radius .
    Sample Input 0
    
    2.6
     
    Sample Output 0
    
    21.237166338267002
    16.336281798666924

---
+++ 10DaysOfJavascript/Day1LetAndConst/LetAndConst.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    //Write your code here. Read input using 'readLine()' and
+    //print output using 'console.log()'.
+
+    //Print the area of the circle:
+    let r, area, perimeter;
+    const PI = Math.PI;
+
+    r = readLine();
+    
+    //Print the area of the circle:
+    area = PI * r * r;
+    console.log(area);
+
+    //Print the perimeter of the circle:
+    perimeter = 2 * PI * r;
+    console.log(perimeter);
+
+    try {
+        //Attempt to redefine the value of constant variable PI
+        PI = 0;
+        //Attempt to print the value of PI
+        console.log(PI);
+    }catch(error) {
+        console.log("You correctly declared 'PI' as a constant");
+    }
+}
