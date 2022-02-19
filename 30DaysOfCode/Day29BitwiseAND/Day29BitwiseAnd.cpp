a4a78efe61aca4d3433b9beb281a77a9ce2b05ac
John I <93291968+ionutziv7@users.noreply.github.com>
2022-02-19 14:12:56 +0200

    Create Day29BitwiseAnd.cpp
    Welcome to the last day! Today, we’re discussing bitwise operations.
    
    Task
    Given set S = {1, 2, 3, . . . ,N}. Find two integers, A and B (where A < B), from set S such that the value of A&B is the maximum possible and also less than a given integer, K. In this case, & represents the bitwise AND operator.
    
    Function Description
    Complete the bitwiseAnd function in the editor below.
    
    bitwiseAnd has the following paramter(s):
    – int N: the maximum integer to consider
    – int K: the limit of the result, inclusive
    
    Returns
    – int: the maximum value of A&B within the limit.
    
    Input Format
    The first line contains an integer, T, the number of test cases.
    Each of the T subsequent lines defines a test case as 2 space-separated integers, N and K, respectively.
    
    Constraints
    1 <= T <= 103
    2 <= N <= 103
    2 <= K <= N
    Sample Input
    
    STDIN   Function
    -----   --------
    3       T = 3
    5 2     N = 5, K = 2
    8 5     N = 8, K = 5
    2 2     N = 8, K = 5
    Sample Output
    
    1
    4
    0

---
+++ 30DaysOfCode/Day29BitwiseAND/Day29BitwiseAnd.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while( t > 0)
+    {
+        int n, k;
+        cin>>n>>k;
+        vector<int>v;
+        
+        for(int i=1;i<=n;i++)
+        {
+            v.push_back(i);
+        }
+        int maximum = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j = i +1 ;j<n;j++)
+            {
+                if( (v[i] & v[j] ) < k && (v[i] & v[j]) > maximum )
+                {
+                    maximum = (v[i] & v[j]);
+                }
+            }
+        }
+        cout<<maximum << endl;
+        t--;
+    }
+	return 0;
+}
