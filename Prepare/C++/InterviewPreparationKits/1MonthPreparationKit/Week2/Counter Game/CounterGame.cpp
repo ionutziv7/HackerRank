d275f706e4c79aa33265db8a973cb14fabe565f7
Ivanciu Ionut <93291968+ionutziv7@users.noreply.github.com>
2022-03-28 11:45:59 +0300

    Create CounterGame.cpp

---
+++ Prepare/C++/InterviewPreparationKits/1MonthPreparationKit/Week2/Counter Game/CounterGame.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		unsigned long long n; int cnt = 0;
+		cin >> n;
+		while (n != 1)
+		{
+			int len = 0;
+			unsigned long long tmp = n;
+			while (tmp != 0) tmp /= 2, len++;
+			unsigned long long a = (unsigned long long)1 << (len - 1);
+			if (n == a) n /= 2;
+			else n -= a;
+			cnt++;
+		}
+		if (cnt % 2 == 0) cout << "Richard" << endl;
+		else cout << "Louise" << endl;
+	}
+	return 0;
+}
